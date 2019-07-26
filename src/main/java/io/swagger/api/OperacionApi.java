86463515e21dc0d50dbcf1bf18cb2f74bdbf5bd6
diego <diego.lopez@iteriam.es>
2019-07-26 11:27:29 +0200

    organizar imports

--- src/main/java/io/swagger/api/OperacionApi.java
+++ src/main/java/io/swagger/api/OperacionApi.java
@@ -5,23 +5,20 @@
  */
 package io.swagger.api;
 
-import io.swagger.model.RestaDatos;
-import io.swagger.model.SumaDatos;
-import io.swagger.annotations.*;
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.model.RestaDatos;
+import io.swagger.model.SumaDatos;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-07-26T07:54:49.951Z")
 
 @Api(value = "operacion", description = "the operacion API")
