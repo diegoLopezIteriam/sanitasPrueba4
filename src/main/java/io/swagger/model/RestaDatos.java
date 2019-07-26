208c9a4e7eb5f376f0db4d5ee927eb226aa37d4a
diego <diego.lopez@iteriam.es>
2019-07-26 12:06:14 +0200

    Validacion POJO resta. Nombres de atributos correctos en resta

--- src/main/java/io/swagger/model/RestaDatos.java
+++ src/main/java/io/swagger/model/RestaDatos.java
@@ -13,53 +13,65 @@ import javax.validation.constraints.*;
  * RestaDatos
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-07-26T07:54:49.951Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-07-26T09:45:06.495Z")
 
 public class RestaDatos   {
-  @JsonProperty("sumando1")
-  private Long sumando1 = null;
 
-  @JsonProperty("sumando2")
-  private Long sumando2 = null;
+  /**
+   * Se definirá la longitud maxima de digitos admitidos. Con decimalMin = 0 impedimos valores negativos
+   * Según la especificación podría ser fuente de conflictos si se restara un numero negativo(suma de valores absolutos)
+   * se debería tener en cuenta
+   */
+  @Digits(integer = 15, fraction = 0)
+  @DecimalMin("0")
+  @NotNull
+  @JsonProperty("minuendo")
+  private Long minuendo = null;
 
-  public RestaDatos sumando1(Long sumando1) {
-    this.sumando1 = sumando1;
+  @Digits(integer = 15, fraction = 0)
+  @DecimalMin("0")
+  @NotNull
+  @JsonProperty("sustraendo")
+  private Long sustraendo = null;
+
+  public RestaDatos minuendo(Long minuendo) {
+    this.minuendo = minuendo;
     return this;
   }
 
   /**
-   * Get sumando1
-   * @return sumando1
-  **/
+   * Get minuendo
+   * @return minuendo
+   **/
   @ApiModelProperty(value = "")
 
 
-  public Long getSumando1() {
-    return sumando1;
+  public Long getMinuendo() {
+    return minuendo;
   }
 
-  public void setSumando1(Long sumando1) {
-    this.sumando1 = sumando1;
+  public void setMinuendo(Long minuendo) {
+    this.minuendo = minuendo;
   }
 
-  public RestaDatos sumando2(Long sumando2) {
-    this.sumando2 = sumando2;
+  public RestaDatos sustraendo(Long sustraendo) {
+    this.sustraendo = sustraendo;
     return this;
   }
 
   /**
-   * Get sumando2
-   * @return sumando2
-  **/
+   * Get sustraendo
+   * @return sustraendo
+   **/
   @ApiModelProperty(value = "")
 
 
-  public Long getSumando2() {
-    return sumando2;
+  public Long getSustraendo() {
+    return sustraendo;
   }
 
-  public void setSumando2(Long sumando2) {
-    this.sumando2 = sumando2;
+  public void setSustraendo(Long sustraendo) {
+    this.sustraendo = sustraendo;
   }
 
 
@@ -72,22 +84,22 @@ public class RestaDatos   {
       return false;
     }
     RestaDatos restaDatos = (RestaDatos) o;
-    return Objects.equals(this.sumando1, restaDatos.sumando1) &&
-        Objects.equals(this.sumando2, restaDatos.sumando2);
+    return Objects.equals(this.minuendo, restaDatos.minuendo) &&
+            Objects.equals(this.sustraendo, restaDatos.sustraendo);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(sumando1, sumando2);
+    return Objects.hash(minuendo, sustraendo);
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class RestaDatos {\n");
-    
-    sb.append("    sumando1: ").append(toIndentedString(sumando1)).append("\n");
-    sb.append("    sumando2: ").append(toIndentedString(sumando2)).append("\n");
+
+    sb.append("    minuendo: ").append(toIndentedString(minuendo)).append("\n");
+    sb.append("    sustraendo: ").append(toIndentedString(sustraendo)).append("\n");
     sb.append("}");
     return sb.toString();
   }
