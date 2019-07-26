50a78760bf09e6e2ecfc9a07144c51afc106b400
diego <diego.lopez@iteriam.es>
2019-07-26 11:41:06 +0200

    Controlador de excepciones para personalizar la respuesta de error
    Se pretende presentar un ejemplo en el que se controlarían las excepciones MethodArgumentNotValidException y se devuelve un 400. Se propondría, por ejemplo, un POJO de error estandar que incluya un codigo interno de error como atributo y otro atributo un mensaje personalizado que concrete de forma explícita y breve el error ocurrido

--- src/main/java/io/swagger/api/ApiException.java
+++ src/main/java/io/swagger/api/ApiException.java
@@ -1,11 +1,31 @@
 package io.swagger.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-07-26T07:54:49.951Z")
 
+@ControllerAdvice
 public class ApiException extends Exception{
-    private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
-        this.code = code;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
+
 }
